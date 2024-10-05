ba2d5cbd1ab5c79dd626c09c85cc684835c8c9e9
winterkeii <johnleomedina@gmail.com>
2024-10-05 12:42:33 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 
-function Button({label, clickHandler, className}){
+function Key({label, clickHandler, className}){
   return(
     <button onClick={clickHandler} className={className}>
       {label}
@@ -72,16 +72,16 @@ function App() {
 
     let result = null;
     if(op === '+'){
-      result = parseFloat(num1) + parseFloat(num2);
+      result = "I MISS YOU <3";
     }
     else if(op === '-'){
-      result = parseFloat(num1) - parseFloat(num2);
+      result = "I MISS YOU <3";
     }
     else if(op === '*'){
-      result = parseFloat(num1) * parseFloat(num2);
+      result = "I MISS YOU <3";
     }
     else if(op === '/'){
-      result = parseFloat(num1) / parseFloat(num2);
+      result = "I MISS YOU <3";
     }
     else{
       result ="Error";
@@ -104,34 +104,41 @@ function App() {
     const value  = e.target.innerHTML;
     console.log(value);
   }
+  const nameclickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    console.log(value);
+    setdisp("HINDI MO BA AKO MISS?");
+  }
 
   return (
     <div className="Calculator">
+      <h1>JOHN LEO MEDINA IT-3A</h1>
       <div className="Calcontainer">
         <div className="Displaycon">
         <Display display ={disp}/>
         </div>
         <div className="Buttons">
-          <Button label={7} clickHandler={numclickHandler}/> 
-          <Button label={8} clickHandler={numclickHandler}/>
-          <Button label={9} clickHandler={numclickHandler}/>
-          <Button label={'/'} clickHandler={opclickHandler}/>
-          <Button label={4} clickHandler={numclickHandler}/>
-          <Button label={5} clickHandler={numclickHandler}/>
-          <Button label={6} clickHandler={numclickHandler}/>
-          <Button label={'*'} clickHandler={opclickHandler}/>
-          <Button label={1} clickHandler={numclickHandler}/>
-          <Button label={2} clickHandler={numclickHandler}/>
-          <Button label={3} clickHandler={numclickHandler}/>
-          <Button label={'-'} clickHandler={opclickHandler}/>
-          <Button label={'.'} clickHandler={numclickHandler}/>
-          <Button label={0} clickHandler={numclickHandler}/>
-          <Button label={'='} clickHandler={eqclickHandler}/>
-          <Button label={'+'} clickHandler={opclickHandler}/>
-          <Button className="clear-button" label={'Clear'} clickHandler={clearclickHandler}/>
-          <Button className="delete-button" label={'c'} clickHandler={numclearclickHandler}/>
+          <Key label={7} clickHandler={numclickHandler}/> 
+          <Key label={8} clickHandler={numclickHandler}/>
+          <Key label={9} clickHandler={numclickHandler}/>
+          <Key label={'/'} clickHandler={opclickHandler}/>
+          <Key label={4} clickHandler={numclickHandler}/>
+          <Key label={5} clickHandler={numclickHandler}/>
+          <Key label={6} clickHandler={numclickHandler}/>
+          <Key label={'*'} clickHandler={opclickHandler}/>
+          <Key label={1} clickHandler={numclickHandler}/>
+          <Key label={2} clickHandler={numclickHandler}/>
+          <Key label={3} clickHandler={numclickHandler}/>
+          <Key label={'-'} clickHandler={opclickHandler}/>
+          <Key label={'.'} clickHandler={numclickHandler}/>
+          <Key label={0} clickHandler={numclickHandler}/>
+          <Key label={'='} clickHandler={eqclickHandler}/>
+          <Key label={'+'} clickHandler={opclickHandler}/>
+          <Key className="clear-Key" label={'Clear'} clickHandler={clearclickHandler}/>
         </div>
       </div>
+      <Key className="MEDINA" label ={'MEDINA'} clickHandler={nameclickHandler}/>
     </div>
   );
 }
